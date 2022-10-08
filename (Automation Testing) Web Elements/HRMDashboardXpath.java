69f8e41cac99a5d1425d6361317debb2d169c3e5
2PrajwalKotian <113607993+2PrajwalKotian@users.noreply.github.com>
2022-10-08 15:28:04 +0530

    Add files via upload

---
+++ (Automation Testing) Web Elements/HRMDashboardXpath.java
@@ -0,0 +1,21 @@
+package finalpresentation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HRMDashboardXpath {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Prajwal\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+			Thread.sleep(1000);
+			driver.findElement(By.name("username")).sendKeys("Admin");
+			driver.findElement(By.name("password")).sendKeys("admin123");
+			driver.findElement(By.xpath("//button[@type='submit']")).click();
+			driver.findElement(By.xpath("//li[3]")).click();
+			driver.findElement(By.xpath("//li[4]")).click();
+			driver.findElement(By.xpath("//li[5]")).click();
+			driver.findElement(By.xpath("//li[6]")).click();
+		}
+		}
---
+++ (Automation Testing) Web Elements/XpathLogin.java
@@ -0,0 +1,16 @@
+package finalpresentation;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class XpathLogin {
+	public static void main(String[]args)throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver", "C:\\Prajwal\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+}
+}
